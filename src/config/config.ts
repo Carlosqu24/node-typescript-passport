3d3aec9938343e6afa771afcb118a7cdb08b8fe7
Carlos Quesada <carlosquesadaumana@gmail.com>
2022-01-22 00:23:50 -0600

    Config added

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,5 +3,6 @@ export default {
             URI: process.env.MONGODB_URI || 'mongodb://localhost/jwt-tutorial',
             USER: process.env.MONGODB_USER || '',
             PASSWORD: process.env.MONGODB_PASSWORD || ''
-      }
+      },
+      jwtSecret: process.env.JWT_SECRET || 'somesecrettoken'
 }
